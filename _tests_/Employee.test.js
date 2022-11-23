379d975d22b2d3a127509c8f784d124ed382a374
mfrabott <mfrabott14@gmail.com>
2022-11-22 23:57:52 -0500

    organize file structure

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -3,6 +3,7 @@ const Employee = require('../lib/Employee')
 describe("Employee", () => {
     describe("getAttributes", () => {
       it("should  create an Employee object and return the employee attributes passed through the input parameters", () => {
+        
         const testName = "Steve";
         const testID = "999"
         const testEmail = 'steve@aol.com'
